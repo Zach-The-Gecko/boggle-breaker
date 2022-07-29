14989e9640a6ee187d8b9bb8c7dfd51cf78989e1
Zach G <Zach.the.gecko@gmail.com>
2022-07-28 20:13:41 -0600

    added cors

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import getAnswers from "./getAnswers.js";
+import cors from "cors";
 import express from "express";
 
-const app = express();
+const app = express(cors());
 
 app.get("/get-answers", (req, res) => {
   const answers = getAnswers(req.query.length, req.query.letters).reduce(
